7632b841d2b2f487a12e8b133560eb9b21d6a8a4
Wellington Souza <111922665+BGWellSS@users.noreply.github.com>
2023-10-23 12:13:14 -0300

    Adding context message for better user understanding

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./index.module.css";
 
 function Home() {
-  return <h1>Vocês são a coisa mais importante da minha vida</h1>;
+  const [hasBeenClicked, setHasBeenClicked] = useState(false);
+
+  const handleImageClick = () => {
+    setHasBeenClicked(true);
+  };
+  const MyIntro = () => {
+    return (
+      <div className={styles.message_image_container}>
+        <img
+          src="https://iili.io/JK7sH8P.md.png"
+          alt="JK7sH8P.md.png"
+          border="0"
+          className={styles.message_image}
+          onClick={handleImageClick}
+        />
+        <h2 className={styles.message_image_label} onClick={handleImageClick}>
+          Clique em mim!
+        </h2>
+      </div>
+    );
+  };
+  const MyMessage = () => {
+    return <h1 className={styles.message_text}>❤️ Eu te amo muito! ❤️</h1>;
+  };
+
+  return (
+    <section className={styles.message_container}>
+      {hasBeenClicked ? <MyMessage /> : <MyIntro />}
+    </section>
+  );
 }
 
 export default Home;
